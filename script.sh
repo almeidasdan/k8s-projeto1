dd1b25447fe49394e6925c9967ffd0407611e794
almeidasdan <almeidas.dan@gmail.com>
2022-12-16 14:20:39 -0300

    Criacao script para deploy aplicacao

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens backend e banco......."
+
+docker build -t almeidadaniel/projeto-backend:1.0 backend/.
+docker build -t almeidadaniel/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens......."
+
+docker push almeidadaniel/projeto-backend:1.0
+docker push almeidadaniel/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes......."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os Deployments......."
+
+kubectl apply -f ./deployment.yml
